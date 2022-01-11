18a40a7f89b0e933b645b48ea404f00f598a2111
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-01-11 14:03:56 +0900

    220111 두 정수 사이의 합 Commit

---
+++ 220111.py
@@ -0,0 +1,20 @@
+# 두 정수 사이의 합
+def solution(a,b):
+    answer = 0
+    if a > b :
+        a,b = b,a
+
+    for i in range(a,b+1):
+        answer += i
+
+    return answer
+
+#Test Case
+ret = solution(3,5)
+print(ret)
+
+ret = solution(3,3)
+print(ret)
+
+ret = solution(5,3)
+print(ret)
