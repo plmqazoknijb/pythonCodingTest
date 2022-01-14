25f6306e5127961f59a410a283071e052e6a1f18
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-01-14 23:58:34 +0900

    220114 약수의 합 Commit

---
+++ 220114.py
@@ -0,0 +1,13 @@
+# 약수의 합
+def solution(n):
+    answer =0
+    for i in range(1,n+1):
+        if n % i == 0:
+            answer += i
+    return answer
+
+# Test Case
+ret = solution(12)
+print(ret)
+ret = solution(5)
+print(ret)
