959185651077a7ad76b01c2ca5876f7bd69a326f
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-01-22 23:56:30 +0900

    220122 이상한 문자 만들기 Commit

---
+++ 220122.py
@@ -0,0 +1,19 @@
+# 이상한 문자 만들기
+def solution(s):
+    s_split = s.split(" ")
+    for k in range(len(s_split)):
+        s_list = list(s_split[k])
+
+        for i in range(len(s_list)):
+            if i % 2 == 0:
+                s_list[i] = s_list[i].upper()
+            elif i % 2 == 1:
+                s_list[i] = s_list[i].lower()
+        s_split[k] = "".join(s_list)
+    answer = " ".join(s_split)
+
+    return answer
+
+# Test Case
+ret = solution("try hello world")
+print(ret)
