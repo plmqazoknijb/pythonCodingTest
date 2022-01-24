a10428d6b935528bb2c0d1d252045573edd4a049
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-01-24 11:36:17 +0900

    220124 콜라츠 추측 Commit

---
+++ 220124.py
@@ -0,0 +1,24 @@
+# 콜라츠 추측
+def solution(num):
+    cnt = 0
+    while True:
+        if num == 1:
+            break
+        if cnt == 500:
+            break
+        if num % 2 == 0:
+            num //= 2
+            cnt+=1
+        elif num % 2 == 1:
+            num = num *3 +1
+            cnt+=1
+
+    return cnt if cnt != 500 else -1
+
+# Test Case
+ret = solution(6)
+print(ret)
+ret = solution(16)
+print(ret)
+ret = solution(-1)
+print(ret)
