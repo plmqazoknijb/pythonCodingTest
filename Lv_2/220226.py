31b4421613d31114950b1d67024b186a6bace498
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-02-26 22:22:03 +0900

    220226 소수 찾기 Commit

---
+++ Lv_2/220226.py
@@ -0,0 +1,16 @@
+# 소수 찾기
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
+
+# Test Case
+ret = solution("17")
+print(ret)
+ret = solution("011")
+print(ret)
