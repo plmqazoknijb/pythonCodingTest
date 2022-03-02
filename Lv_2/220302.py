c292973fd90194eea80187991a506e9eee53649b
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-02 23:50:53 +0900

    220302 행렬의 곱셈 Commit

---
+++ Lv_2/220302.py
@@ -0,0 +1,14 @@
+# 행렬의 곱셈
+def solution(arr1, arr2):
+    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr1[0])):
+                answer[i][j] += (arr1[i][k] * arr2[k][j])
+    return answer
+
+# Test Case
+ret = solution([[1, 4], [3, 2], [4, 1]],[[3, 3], [3, 3]])
+print(ret)
+ret = solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]],[[5, 4, 3], [2, 4, 1], [3, 1, 1]])
+print(ret)
