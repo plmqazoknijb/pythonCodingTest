87776990547d31ec931194eb87a1495eee956b03
은원 <plmqazoknijb@naver.com>
2022-04-14 23:16:30 +0900

    220414 프렌즈4블록 Commit

---
+++ Lv_2/220414.py
@@ -0,0 +1,28 @@
+# 프렌즈4블록
+def pop_num(b, m, n):
+    pop_set = set()
+    for i in range(1, n):
+        for j in range(1, m):
+            if b[i][j] == b[i - 1][j - 1] == b[i - 1][j] == b[i][j - 1] != '_':
+                pop_set |= set([(i, j), (i - 1, j - 1), (i - 1, j), (i, j - 1)])
+    for i, j in pop_set:
+        b[i][j] = 0
+    for i, row in enumerate(b):
+        empty = ['_'] * row.count(0)
+        b[i] = empty + [block for block in row if block != 0]
+    return len(pop_set)
+
+
+def solution(m, n, board):
+    count = 0
+    b = list(map(list, zip(*board)))
+    while True:
+        pop = pop_num(b, m, n)
+        if pop == 0: return count
+        count += pop
+
+# Test Case
+ret = solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"])
+print(ret)
+ret = solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"])
+print(ret)
