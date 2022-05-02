cffbb4a163afbbfcb513fb37508a3e73bbea79ab
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-05-02 12:04:07 +0900

    220502 짝지어 제거하기 Commit

---
+++ Lv_2/220502.py
@@ -0,0 +1,15 @@
+# 짝지어 제거하기
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0: stack.append(i)
+        elif stack[-1] == i: stack.pop()
+        else: stack.append(i)
+    if len(stack) == 0: return 1
+    else: return 0
+
+# Test Case
+ret = solution("baabaa")
+print(ret)
+ret = solution("cdcd")
+print(ret)
