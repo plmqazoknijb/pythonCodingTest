d34167e1869375c684d4c5b48a6f8b090d6a3600
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-05-08 19:10:12 +0900

    220508 오픈채팅방 Commit

---
+++ Lv_2/220508.py
@@ -0,0 +1,20 @@
+# 오픈채팅방
+def solution(record):
+    answer = []
+    userdict = {}
+    for mes in record:
+        if (mes.split(' ')[0] == 'Enter') | (mes.split(' ')[0] == 'Change'):
+            userdict[mes.split(' ')[1]] = mes.split(' ')[2]
+
+    for mes in record:
+        if mes.split(' ')[0] == 'Enter':
+            answer.append("{}님이 들어왔습니다.".format(userdict[mes.split(' ')[1]]))
+        elif mes.split(' ')[0] == 'Leave':
+            answer.append("{}님이 나갔습니다.".format(userdict[mes.split(' ')[1]]))
+        else:
+            pass
+    return answer
+
+# Test Case
+ret = solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
+print(ret)
