e34c53e8cc1fb142f00313498a483eca0f45007a
은원 <plmqazoknijb@naver.com>
2022-05-23 23:17:36 +0900

    220523 줄 서는 방법 Commit

---
+++ Lv_3/220523.py
@@ -0,0 +1,20 @@
+# 줄 서는 방법
+import math
+
+def solution(n, k):
+    answer = []
+    numberList = [i for i in range(1, n + 1)]
+    while (n != 0):
+        temp = math.factorial(n) // n  # 한개에 몇개씩의 값이 있을지 알 수 잇음.
+        index = k // temp
+        k = k % temp
+        if k == 0:
+            answer.append(numberList.pop(index - 1))
+        else:
+            answer.append(numberList.pop(index))
+        n -= 1
+    return answer
+
+# Test Case
+ret = solution(3,5)
+print(ret)
