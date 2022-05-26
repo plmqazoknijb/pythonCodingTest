e76e4e64a4c5f596d49c6e05b95cb6e414ca1517
은원 <plmqazoknijb@naver.com>
2022-05-26 22:41:55 +0900

    220526 입국심사 Commit

---
+++ Lv_3/220526.py
@@ -0,0 +1,22 @@
+# 입국심사
+def solution(n, times):
+    answer = 0
+    left, right = 1, max(times) * n
+    while left <= right:
+        mid = (left + right) // 2
+        people = 0
+        for time in times:
+            people += mid // time
+            if people >= n:
+                break
+        if people >= n:
+            answer = mid
+            right = mid - 1
+        elif people < n:
+            left = mid + 1
+
+    return answer
+
+# Test Case
+ret = solution(6,[7,10])
+print(ret)
